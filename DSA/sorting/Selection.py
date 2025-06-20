0e46392e3fdc02fcbf7596c6d4c03fa16a7f29d5
NichuthReddy <nichuthreddy@gmail.com>
2025-06-20 09:16:43 +0530

    sort change

--- DSA/sorting/Selection.py
+++ DSA/sorting/Selection.py
@@ -1,14 +1,14 @@
 class Solution:
     def selectionSort(self, nums):
         len_nums=len(nums)
-        for i in range(0,len_nums+1):
-            for j in range(1,len_nums+1):
-                if nums[i]<nums[j]:
+        for i in range(0,len_nums-1):
+            for j in range(i+1,len_nums):
+                if nums[i]>nums[j]:
                     nums[i],nums[j]=nums[j],nums[i]
         return nums
 
-    selectsort = Solution
+selectsort = Solution()
 
-    nums = [5, 2, 9, 1, 5, 6, 3] #testing
-    
-    selectsort.selectionsort(nums)
+nums = [5, 2, 9, 1, 5, 6, 3, 10] #testing
+print("Testing Selection Sort")
+print(selectsort.selectionSort(nums))
